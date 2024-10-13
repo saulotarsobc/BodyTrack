8f8f1880e246a6b071427da64f819d07e317cb78
saulotarsobc <saulotarsobc@gmail.com>
2024-10-13 16:06:23 -0300

    :fire:

--- init.sh
+++ init.sh
@@ -3,7 +3,6 @@ echo -e "\n\nRemoving old dependencies...\n";
 deactivate;
 rm -rf .ven;
 
-
 echo -e "\n\nInstalling dependencies...\n";
 python3 -m venv .venv;
 source .venv/Scripts/activate;
@@ -11,14 +10,6 @@ python.exe -m pip install --upgrade pip;
 
 pip install -q mediapipe;
 pip install opencv-python;
-# pip install -r requirements.txt;
-
-
-echo -e "\n\nCloning MediaPipe...\n";
-git clone https://github.com/google/mediapipe.git
-
-# delelte .git folder from mediapipe
-rm -rf mediapipe/.git
 
 echo -e "\n\nAll dependencies are installed.\nYou can now run the app by running 'python main.py'.\n\n";
 # pip install mediapipe opencv-python pyinstaller;
